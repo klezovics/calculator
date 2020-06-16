de8bfdab58c2368345e3565c9767249717e4c9aa
aklezovics <arturs.klezovics@raisin.com>
2020-06-16 13:25:18 +0200

    Initial commit

---
+++ src/com/klezovich/Calculator.java
@@ -0,0 +1,20 @@
+package com.klezovich;
+
+
+public class Calculator {
+
+    // Tasks
+    // - Add unit tests for addition
+    // - Add unit tests for substraction
+    // - Implement multiplication and add unit tests
+    // - Implement division and add unit tests
+
+    int add(int x, int y) {
+        return x+y;
+    }
+
+    int substact(int x, int y) {
+        return x-y;
+    }
+
+}
